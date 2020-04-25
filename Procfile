34d45a0228f51e9cf6e9f38bba1f852619c3ce84
GustC <gustavogu44@hotmail.com>
2020-04-25 18:08:41 -0300

    Ajustes

--- Procfile
+++ Procfile
@@ -1,3 +1,6 @@
 FLASK_APP=projetoSD.py flask run
 
 web: python projetoSD.py
+init: python manage.py db init
+migrate: python manage.py db migrate
+upgrade: python manage.py db upgrade
--- app/config.py
+++ app/config.py
@@ -1,15 +1,17 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+SQL_HOST = "dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+SQL_PORT = "3306"
+SQL_USERNAME = "frckdq80jtcrhe01"
+SQL_PASSWORD = "w2wlr0b87yq3rvox"
+SQL_DATABASE_NAME = "refkiyyw8og41pag" 
+
 class Config(object):
     # url = "mysql:///root:[1234]@[HOST]:[PORT]/[DB_NAME]"
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #     'sqlite:///' + os.path.join(basedir, 'app2.db')
-    SQL_HOST = "dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
-    SQL_PORT = "3306"
-    SQL_USERNAME = "frckdq80jtcrhe01"
-    SQL_PASSWORD = "w2wlr0b87yq3rvox"
-    SQL_DATABASE_NAME = "refkiyyw8og41pag" 
+    
     SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}:{}/{}".format(SQL_USERNAME,SQL_PASSWORD,SQL_HOST,SQL_PORT,SQL_DATABASE_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
