9eeaadebb93a96952d77e941e9a6d1781f20de91
GustC <gustavogu44@hotmail.com>
2020-04-25 18:53:45 -0300

    Correções

--- projetoSD.py
+++ projetoSD.py
@@ -3,4 +3,5 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.run(host='0.0.0.0', port=port)
