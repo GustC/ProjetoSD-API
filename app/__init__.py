f97cdacae8ac4c745e29934f6fc656ea2a487111
GustC <gustavogu44@hotmail.com>
2020-04-28 16:19:23 -0300

    Correção

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
  
-os.environ["DATABASE_URL"] = "postgres://klshqakpumicqw:b81648d62aba36df00aac43c2ae849a9e238bf07ca3a266eb956ddfc96f75c8c@ec2-52-86-73-86.compute-1.amazonaws.com:5432/dcjvpvavero43d"
+# os.environ["DATABASE_URL"] = "postgres://klshqakpumicqw:b81648d62aba36df00aac43c2ae849a9e238bf07ca3a266eb956ddfc96f75c8c@ec2-52-86-73-86.compute-1.amazonaws.com:5432/dcjvpvavero43d"
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
